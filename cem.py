bccb3842f1eee9e88591b2e960584261548f121d
Stefan <hn19405@bristol.ac.uk>
2021-05-20 15:20:53 +0100

    cem+grad implemented but performing on pendulum

---
+++ cem.py
@@ -0,0 +1,97 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import wandb
+from tqdm import tqdm
+from RL_framework.common.networks import SequentialNetwork
+from RL_framework.common.buffer import ReplayMemory
+from RL_framework.common.gymenv import GymEnv
+from RL_framework.common.model_based import DynamicsModel, MPC
+
+# Environment details
+# ~~~~~~~~~~~~~~~~~~~
+env = GymEnv('Pendulum-v0')
+
+# General details
+# ~~~~~~~~~~~~~~~
+wandb.init(project='framework_pendulum')
+wandb.config.algorithm = 'CEM'
+num_episodes = 50
+gamma = 0.9
+params = {'sample_collection': 1,
+          'buffer_size': 5000,
+          'minibatch_size': 32,
+          'training_epoch': 5,
+          'control_horizon': 20,
+          'K_actions_sample': 50,
+          'Best_K': 10,
+          'grad': True
+          }
+wandb.config.gamma = gamma
+wandb.config.update(params)
+
+# Networks details
+# ~~~~~~~~~~~~~~~~
+network_layers = {'model_layers': [nn.Linear(env.obs_size + env.action_size, 32),
+                                   nn.ReLU(),
+                                   nn.Linear(32, 64),
+                                   nn.ReLU(),
+                                   nn.Linear(64, 64),
+                                   nn.ReLU(),
+                                   nn.Linear(64, env.obs_size)],
+                  'reward_layers': [nn.Linear(env.obs_size + env.action_size, 32),
+                                    nn.ReLU(),
+                                    nn.Linear(32, 64),
+                                    nn.ReLU(),
+                                    nn.Linear(64, 64),
+                                    nn.ReLU(),
+                                    nn.Linear(64, 1)]
+                  }
+learning_rates = dict(model_lr=1e-3, reward_lr=1e-3)
+model_loss_fnc = torch.nn.MSELoss()
+wandb.config.update(network_layers)
+wandb.config.update(learning_rates)
+
+# Initialisation
+# ~~~~~~~~~~~~~~
+buffer = ReplayMemory(params['buffer_size'])
+
+model_net = SequentialNetwork(network_layers['model_layers'])
+model_opt = optim.Adam(model_net.parameters(), lr=learning_rates['model_lr'])
+reward_net = SequentialNetwork(network_layers['reward_layers'])
+rew_opt = optim.Adam(reward_net.parameters(), lr=learning_rates['reward_lr'])
+dynamics = DynamicsModel(model_net, buffer, model_loss_fnc, model_opt, env, 'diff', reward_net, rew_opt)
+MPC = MPC(model_net, env, gamma, reward_net)
+
+# Gather data and training
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+global_step = buffer.populate_randomly(env, 0.1)
+dynamics.train_model(20, params['minibatch_size'], noise_std=0.001)
+dynamics.train_reward_fnc(20, params['minibatch_size'])
+
+for episode in tqdm(range(num_episodes)):
+    episode_step = 0
+    episode_reward = 0
+    state = env.env.reset()
+    terminal = False
+    while terminal is False:
+        action = MPC.cem_planning(state, params['K_actions_sample'], params['control_horizon'], params['Best_K'],
+                                  episode_step, keep_best_k=True, grad=params['grad'])
+        action_scaled = (env.action_low + (env.action_high - env.action_low) * (action + 1) / 2).numpy()
+        next_state, reward, terminal, _ = env.env.step(action_scaled)
+        wandb.log({'reward': reward, 'step': global_step, 'episode': episode})
+        episode_reward += reward
+        episode_step += 1
+        global_step += 1
+        buffer.add(state, action, reward, next_state, terminal, None, None)
+        state = next_state
+
+        if (episode_step % params['sample_collection'] == 0 or terminal is True) and\
+                len(buffer) >= params['minibatch_size']:
+
+            # Train dynamics model
+            # ~~~~~~~~~~~~~~~~~~~~
+            dynamics.train_model(params['training_epoch'], params['minibatch_size'], noise_std=0.001)
+            dynamics.train_reward_fnc(params['training_epoch'], params['minibatch_size'])
+
+    wandb.log({"episode_reward": episode_reward, 'episode': episode})
--- common/model_based.py
+++ common/model_based.py
@@ -1,16 +1,20 @@
 import torch
+from torch.distributions.normal import Normal
+import torch.optim as optim
 import wandb
 from RL_framework.common.buffer import ProcessMinibatch
 
 
 class DynamicsModel:
-    def __init__(self, model, buffer, loss_func, opt, env, model_type='diff'):
+    def __init__(self, model, buffer, loss_func, opt, env, model_type='diff', reward=None, rew_opt=None):
         self.model = model
         self.buffer = buffer
         self.loss_func = loss_func
         self.opt = opt
         self.env = env
         self.type = model_type
+        self.reward = reward
+        self.rew_opt = rew_opt
 
     def train_model(self, epochs, minibatch_size, standardise=False, noise_std=None):
 
@@ -23,7 +27,7 @@ class DynamicsModel:
 
             if self.type == 'forward':
                 target = t.next_states
-            elif self.type == 'diff':
+            else:
                 target = t.next_states - t.states
 
             if noise_std is not None:
@@ -39,29 +43,85 @@ class DynamicsModel:
             loss.backward()
             self.opt.step()
 
+    def train_reward_fnc(self, epochs, minibatch_size):
+
+        for i in range(epochs):
+            minibatch = self.buffer.random_sample(minibatch_size)
+            t = ProcessMinibatch(minibatch)
+            target = t.rewards
+            current = self.reward(torch.cat((t.states, t.actions), dim=1))
+            loss = self.loss_func(current, target)
+            wandb.log({"reward_loss": loss}, commit=False)
+            self.rew_opt.zero_grad()
+            loss.backward()
+            self.rew_opt.step()
+
 
 class MPC:
-    def __init__(self, model, env, gamma):
-        self.model = model
+    def __init__(self, model_net, env, gamma, reward_net=None):
+        self.model = model_net
         self.env = env
         self.gamma = gamma
+        self.reward_net = reward_net
+        self.action_sequence = None
 
-    def random_shooting(self, state, k, horizon, normalise_state=True):
-        samples = torch.randint(self.env.action_size, size=(k, horizon))
-        rewards = torch.zeros(k)
-        for i, sample in enumerate(samples):
-            current_state = torch.Tensor(state)
-            if normalise_state:
-                current_state /= self.env.obs_high
-            reward = 0
-            for j, action in enumerate(sample):
-                state_action = torch.cat((current_state, action.reshape(1)))
+    def _rollout(self, state, samples, normalise, grad=False):
+        rewards = torch.zeros(len(samples))
+        state = torch.ones(len(samples), len(state)) * torch.Tensor(state)
+
+        if normalise:
+            state /= self.env.obs_high
+
+        for t in range(samples.shape[1]):
+            action = samples[:, t]
+            if grad is False:
                 with torch.no_grad():
-                    current_state += self.model(state_action)
-                if normalise_state:
-                    reward += self.gamma ** j * self.env.reward_func(current_state * self.env.obs_high)
-                else:
-                    reward += self.gamma ** j * self.env.reward_func(current_state)
-            rewards[i] = reward
+                    state += self.model(torch.cat((state, action), dim=1))
+                rewards += torch.Tensor([self.gamma ** t * self.env.reward_func(state[i], action[i])
+                                         for i in range(len(state))])
+            else:
+                rewards.requires_grad_()
+                state += self.model(torch.cat((state, action), dim=1))
+                rewards = rewards.add(self.gamma ** t * self.reward_net(torch.cat((state, action), dim=1)).squeeze())
+
+        return rewards
+
+    def random_shooting(self, state, k, horizon, normalise_state=False):
+        samples = torch.randint(self.env.action_size, size=(k, horizon))
+        rewards = self._rollout(state, samples, normalise_state)
         best_k = torch.argmax(rewards)
         return samples[best_k, 0].item()
+
+    def cem_planning(self, state, k, horizon, best_k, episode_step, keep_best_k=False, cem_iters=10, alpha=0.6,
+                     normalise_state=False, grad=False, grad_iters=5):
+
+        if episode_step == 0:
+            mu = torch.zeros(horizon, self.env.action_size)
+        else:
+            mu = torch.cat((self.action_sequence[1:], torch.Tensor([[0]])))
+        sigma = torch.ones_like(mu) / 2
+
+        for i in range(cem_iters):
+            action_dis = Normal(mu, sigma)
+            if keep_best_k is True and i != 0:
+                samples = torch.cat((best_samples, torch.clamp(action_dis.sample(((k - best_k),)), -1, 1)))
+            else:
+                samples = torch.clamp(action_dis.sample((k,)), -1, 1)
+
+            if grad is True:
+                samples.requires_grad_()
+                action_opt = optim.Adam([samples], lr=0.05)
+                for _ in range(grad_iters):
+                    action_opt.zero_grad()
+                    rewards = self._rollout(state, samples, normalise_state, grad=grad).sum()
+                    (-rewards).backward()
+                    action_opt.step()
+                samples = samples.detach()
+
+            rewards = self._rollout(state, samples, normalise_state, grad=False)
+            best_samples = samples[rewards.sort(descending=True).indices[:best_k]]
+            mu_elite, sigma_elite = torch.std_mean(best_samples, dim=0)
+            mu = alpha * mu + (1 - alpha) * mu_elite
+            sigma = alpha * sigma + (1 - alpha) * sigma_elite
+        self.action_sequence = mu
+        return mu[0]
